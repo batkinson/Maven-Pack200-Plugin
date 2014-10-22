a17da95562b9cbf49ef93dea8a8baca0b2d5f2a5
Brent Atkinson <brent.atkinson@gmail.com>
2014-10-21 20:45:19 -0400

    Reformatted code with intellij default formatting.

--- src/main/java/org/openxdata/plugins/PackMojo.java
+++ src/main/java/org/openxdata/plugins/PackMojo.java
@@ -41,194 +41,193 @@ import org.codehaus.plexus.util.IOUtil;
 
 /**
  * Goal capable of compressing a jar with the pack200 tool.
- * 
+ *
  * @goal pack
- * 
  * @phase package
  */
 public class PackMojo extends AbstractMojo {
 
-	/**
-	 * @parameter default-value="${project}"
-	 * @required
-	 * @readonly
-	 */
-	MavenProject project;
+    /**
+     * @parameter default-value="${project}"
+     * @required
+     * @readonly
+     */
+    MavenProject project;
 
-	/**
-	 * @parameter default-value=""
-	 */
-	private boolean processMainArtifact;
+    /**
+     * @parameter default-value=""
+     */
+    private boolean processMainArtifact;
 
-	/**
-	 * @parameter default-value=""
-	 */
-	private Set<String> includeClassifiers;
+    /**
+     * @parameter default-value=""
+     */
+    private Set<String> includeClassifiers;
 
-	/**
-	 * @parameter default-value="true"
-	 */
-	private boolean attachPackedArtifacts;
+    /**
+     * @parameter default-value="true"
+     */
+    private boolean attachPackedArtifacts;
 
-	/**
-	 * @parameter default-value="false"
-	 */
-	private boolean normalizeOnly;
+    /**
+     * @parameter default-value="false"
+     */
+    private boolean normalizeOnly;
 
-	/**
-	 * @parameter default-value="7"
-	 */
-	private String effort;
+    /**
+     * @parameter default-value="7"
+     */
+    private String effort;
 
-	/**
-	 * @parameter default-value="-1"
-	 */
-	private String segmentLimit;
+    /**
+     * @parameter default-value="-1"
+     */
+    private String segmentLimit;
 
-	/**
-	 * @parameter default-value="false"
-	 */
-	private String keepFileOrder;
+    /**
+     * @parameter default-value="false"
+     */
+    private String keepFileOrder;
 
-	/**
-	 * @parameter default-value="latest"
-	 */
-	private String modificationTime;
+    /**
+     * @parameter default-value="latest"
+     */
+    private String modificationTime;
 
-	/**
-	 * @parameter default-value="false"
-	 */
-	private String deflateHint;
+    /**
+     * @parameter default-value="false"
+     */
+    private String deflateHint;
 
-	/**
-	 * @parameter
-	 */
-	private String[] stripCodeAttributes = { "SourceFile", "LineNumberTable",
-			"LocalVariableTable", "Deprecated" };
+    /**
+     * @parameter
+     */
+    private String[] stripCodeAttributes = {"SourceFile", "LineNumberTable",
+            "LocalVariableTable", "Deprecated"};
 
-	/**
-	 * @parameter default-value="true"
-	 */
-	private boolean failOnUnknownAttributes;
+    /**
+     * @parameter default-value="true"
+     */
+    private boolean failOnUnknownAttributes;
 
-	/**
-	 * @parameter default-value="true"
-	 */
-	private boolean compress;
+    /**
+     * @parameter default-value="true"
+     */
+    private boolean compress;
 
-	@SuppressWarnings("unchecked")
-	public void execute() throws MojoExecutionException {
+    @SuppressWarnings("unchecked")
+    public void execute() throws MojoExecutionException {
 
-		getLog().debug("starting pack");
+        getLog().debug("starting pack");
 
-		Packer packer = Pack200.newPacker();
-		Unpacker unpacker = null;
+        Packer packer = Pack200.newPacker();
+        Unpacker unpacker = null;
 
-		if (normalizeOnly)
-			unpacker = Pack200.newUnpacker();
+        if (normalizeOnly)
+            unpacker = Pack200.newUnpacker();
 
-		String type = "jar.pack";
-		if (compress)
-			type = "jar.pack.gz";
+        String type = "jar.pack";
+        if (compress)
+            type = "jar.pack.gz";
 
-		Map<String, String> packerProps = packer.properties();
+        Map<String, String> packerProps = packer.properties();
 
-		packerProps.put(Packer.EFFORT, effort);
-		packerProps.put(Packer.SEGMENT_LIMIT, segmentLimit);
-		packerProps.put(Packer.KEEP_FILE_ORDER, keepFileOrder);
-		packerProps.put(Packer.MODIFICATION_TIME, modificationTime);
-		packerProps.put(Packer.DEFLATE_HINT, deflateHint);
+        packerProps.put(Packer.EFFORT, effort);
+        packerProps.put(Packer.SEGMENT_LIMIT, segmentLimit);
+        packerProps.put(Packer.KEEP_FILE_ORDER, keepFileOrder);
+        packerProps.put(Packer.MODIFICATION_TIME, modificationTime);
+        packerProps.put(Packer.DEFLATE_HINT, deflateHint);
 
-		if (stripCodeAttributes != null) {
-			for (String attributeName : stripCodeAttributes) {
-				getLog().debug("stripping " + attributeName);
-				packerProps.put(Packer.CODE_ATTRIBUTE_PFX + attributeName,
-						Packer.STRIP);
-			}
-		}
+        if (stripCodeAttributes != null) {
+            for (String attributeName : stripCodeAttributes) {
+                getLog().debug("stripping " + attributeName);
+                packerProps.put(Packer.CODE_ATTRIBUTE_PFX + attributeName,
+                        Packer.STRIP);
+            }
+        }
 
-		if (failOnUnknownAttributes)
-			packerProps.put(Packer.UNKNOWN_ATTRIBUTE, Packer.ERROR);
+        if (failOnUnknownAttributes)
+            packerProps.put(Packer.UNKNOWN_ATTRIBUTE, Packer.ERROR);
 
-		List<Artifact> artifactsToPack = new ArrayList<Artifact>();
+        List<Artifact> artifactsToPack = new ArrayList<Artifact>();
 
-		if (processMainArtifact)
-			artifactsToPack.add(project.getArtifact());
+        if (processMainArtifact)
+            artifactsToPack.add(project.getArtifact());
 
-		if (includeClassifiers != null && !includeClassifiers.isEmpty()) {
-			for (Artifact secondaryArtifact : (List<Artifact>) project
-					.getAttachedArtifacts())
-				if ("jar".equals(secondaryArtifact.getType())
-						&& secondaryArtifact.hasClassifier()
-						&& includeClassifiers.contains(secondaryArtifact
-								.getClassifier()))
-					artifactsToPack.add(secondaryArtifact);
-		}
+        if (includeClassifiers != null && !includeClassifiers.isEmpty()) {
+            for (Artifact secondaryArtifact : (List<Artifact>) project
+                    .getAttachedArtifacts())
+                if ("jar".equals(secondaryArtifact.getType())
+                        && secondaryArtifact.hasClassifier()
+                        && includeClassifiers.contains(secondaryArtifact
+                        .getClassifier()))
+                    artifactsToPack.add(secondaryArtifact);
+        }
 
-		String action = normalizeOnly ? "normalizing" : "packing";
+        String action = normalizeOnly ? "normalizing" : "packing";
 
-		getLog().info(action + " " + artifactsToPack.size() + " artifacts");
+        getLog().info(action + " " + artifactsToPack.size() + " artifacts");
 
-		for (Artifact artifactToPack : artifactsToPack) {
+        for (Artifact artifactToPack : artifactsToPack) {
 
-			File origFile = artifactToPack.getFile();
-			File packFile = new File(origFile.getAbsolutePath() + ".pack");
-			File zipFile = new File(packFile.getAbsolutePath() + ".gz");
+            File origFile = artifactToPack.getFile();
+            File packFile = new File(origFile.getAbsolutePath() + ".pack");
+            File zipFile = new File(packFile.getAbsolutePath() + ".gz");
 
-			try {
-				JarFile jar = new JarFile(origFile);
-				FileOutputStream fos = new FileOutputStream(packFile);
+            try {
+                JarFile jar = new JarFile(origFile);
+                FileOutputStream fos = new FileOutputStream(packFile);
 
-				getLog().debug("packing " + origFile + " to " + packFile);
-				packer.pack(jar, fos);
+                getLog().debug("packing " + origFile + " to " + packFile);
+                packer.pack(jar, fos);
 
-				getLog().debug("closing handles ...");
-				jar.close();
-				fos.close();
+                getLog().debug("closing handles ...");
+                jar.close();
+                fos.close();
 
-				if (normalizeOnly) {
-					getLog().debug("unpacking " + packFile + " to " + origFile);
-					JarOutputStream origJarStream = new JarOutputStream(
-							new FileOutputStream(origFile));
-					unpacker.unpack(packFile, origJarStream);
+                if (normalizeOnly) {
+                    getLog().debug("unpacking " + packFile + " to " + origFile);
+                    JarOutputStream origJarStream = new JarOutputStream(
+                            new FileOutputStream(origFile));
+                    unpacker.unpack(packFile, origJarStream);
 
-					getLog().debug("closing handles...");
-					origJarStream.close();
+                    getLog().debug("closing handles...");
+                    origJarStream.close();
 
-					getLog().debug("unpacked file");
-				} else {
+                    getLog().debug("unpacked file");
+                } else {
 
-					Artifact newArtifact = new DefaultArtifact(
-							artifactToPack.getGroupId(),
-							artifactToPack.getArtifactId(),
-							artifactToPack.getVersionRange(),
-							artifactToPack.getScope(), type,
-							artifactToPack.getClassifier(),
-							new DefaultArtifactHandler(type));
+                    Artifact newArtifact = new DefaultArtifact(
+                            artifactToPack.getGroupId(),
+                            artifactToPack.getArtifactId(),
+                            artifactToPack.getVersionRange(),
+                            artifactToPack.getScope(), type,
+                            artifactToPack.getClassifier(),
+                            new DefaultArtifactHandler(type));
 
-					if (compress) {
-						getLog().debug(
-								"compressing " + packFile + " to " + zipFile);
-						GZIPOutputStream zipOut = new GZIPOutputStream(
-								new FileOutputStream(zipFile));
-						IOUtil.copy(new FileInputStream(packFile), zipOut);
-						zipOut.close();
-						newArtifact.setFile(zipFile);
-					} else {
-						newArtifact.setFile(packFile);
-					}
+                    if (compress) {
+                        getLog().debug(
+                                "compressing " + packFile + " to " + zipFile);
+                        GZIPOutputStream zipOut = new GZIPOutputStream(
+                                new FileOutputStream(zipFile));
+                        IOUtil.copy(new FileInputStream(packFile), zipOut);
+                        zipOut.close();
+                        newArtifact.setFile(zipFile);
+                    } else {
+                        newArtifact.setFile(packFile);
+                    }
 
-					if (attachPackedArtifacts) {
-						getLog().debug("attaching " + newArtifact);
-						project.addAttachedArtifact(newArtifact);
-					}
-				}
+                    if (attachPackedArtifacts) {
+                        getLog().debug("attaching " + newArtifact);
+                        project.addAttachedArtifact(newArtifact);
+                    }
+                }
 
-				getLog().debug("finished " + action + " " + packFile);
+                getLog().debug("finished " + action + " " + packFile);
 
-			} catch (IOException e) {
-				throw new MojoExecutionException("Failed to pack jar.", e);
-			}
-		}
-	}
+            } catch (IOException e) {
+                throw new MojoExecutionException("Failed to pack jar.", e);
+            }
+        }
+    }
 }
